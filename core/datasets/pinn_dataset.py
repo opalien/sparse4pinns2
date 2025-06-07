d4c71a2a955b6b7e99ec3a9e87573ac51e42eaad
Tib <NotAn@ema.il>
2025-06-07 14:28:00 +0200

    type naming changed

--- core/datasets/pinn_dataset.py
+++ core/datasets/pinn_dataset.py
@@ -4,65 +4,67 @@ from typing import Literal
 
 
 
-element_type = tuple[Literal[0],tuple[Tensor, Tensor]]
-periodic_type = tuple[Literal[1], list[Tensor]]
-colloc_type = tuple[Literal[2], Tensor]
+dirichlet_type = tuple[Tensor, Tensor]
+periodic_type = tuple[Tensor, Tensor]
+colloc_type = Tensor
 
-TYPE = element_type | periodic_type | colloc_type
-
+TYPE = tuple[Literal[0], dirichlet_type] | tuple[Literal[1], periodic_type] | tuple[Literal[2], colloc_type]
 
 
 PINNDataloader = torch.utils.data.DataLoader[TYPE]
 
 class PINNDataset(torch.utils.data.Dataset[TYPE]):
-    def __init__(self):
+    def __init__(self, device: torch.device):
         super().__init__()
 
+        self.device = device
+
         # Bounds conditions
-        # u(elements[:][0]) = elements[:][1]
-        self.elements: list[ tuple[Tensor, Tensor] ] = []
+        # u(dirichlet[:][0]) = dirichlet[:][1]
+        self.dirichlet: list[dirichlet_type] = []
 
         # Periodic boundary conditions
-        # size of elements of periodic can be different
-        # u(periodic[i][j]) = u(periodic[i][k]), where j != k
-        self.periodic: list[list[Tensor]] = []
+        # size of dirichlet of periodic can be different
+        # u(periodic[:][0]) = u(periodic[:][1])
+        self.periodic: list[periodic_type] = []
 
         # Collocation points such PDE is respected
         # MSE(O(colloc[:])) = 0
-        self.colloc: list[Tensor] = []
+        self.colloc: list[colloc_type] = []
 
 
         self.u_zero: Tensor | None = None
 
 
-    def set_elements(self, elements: list[ tuple[Tensor, Tensor] ]):
-        self.elements = elements
-        if len(self.elements) > 0:
-            self.u_zero = torch.zeros_like(self.elements[0][1])
+    def set_dirichlet(self, dirichlet: list[ dirichlet_type ]):
+        self.dirichlet = dirichlet
+        if len(self.dirichlet) > 0:
+            self.u_zero = torch.zeros_like(self.dirichlet[0][1])
 
 
-    def set_periodic(self, periodic: list[list[Tensor]]):
+    def set_periodic(self, periodic: list[ periodic_type ]):
         self.periodic = periodic
     
 
-    def set_colloc(self, colloc: list[Tensor]):
+    def set_colloc(self, colloc: list[ colloc_type ]):
         self.colloc = colloc
 
     
-    def append_element(self, a:Tensor, u:Tensor):
+    def append_dirichlet(self, a: Tensor, u: Tensor):
         a = torch.as_tensor(a, dtype=torch.float32)
         u = torch.as_tensor(u, dtype=torch.float32)
-        self.elements.append((a, u))
+        self.dirichlet.append((a, u))
         if self.u_zero is None:
             self.u_zero = torch.zeros_like(u)
             
 
-    def append_periodic(self, A: list[Tensor]):
-        A = [torch.as_tensor(a, dtype=torch.float32) for a in A]
-        self.periodic.append(A)
-    
+    def append_periodic(self, a_1: Tensor, a_2: Tensor):
+        a_1 = torch.as_tensor(a_1, dtype=torch.float32)
+        a_2 = torch.as_tensor(a_2, dtype=torch.float32)
+        self.periodic.append((a_1, a_2))
 
-    def append_colloc(self, colloc:Tensor):
+
+    def append_colloc(self, colloc: Tensor):
         colloc = torch.as_tensor(colloc, dtype=torch.float32)
         self.colloc.append(colloc)
 
@@ -70,22 +72,22 @@ class PINNDataset(torch.utils.data.Dataset[TYPE]):
 
 
     def __len__(self):
-        return len(self.elements) + len(self.periodic) + len(self.colloc)
+        return len(self.dirichlet) + len(self.periodic) + len(self.colloc)
 
 
-    # idx -> [type, elements | colloc | periodic]
+    # idx -> [type, dirichlet | colloc | periodic]
     def __getitem__(self, idx: int) -> TYPE:
         match idx:
-            case _ if idx < len(self.elements):
-                a, u = self.elements[idx]
+            case _ if idx < len(self.dirichlet):
+                a, u = self.dirichlet[idx]
                 return 0, (a, u)
             
-            case _ if idx < len(self.elements) + len(self.periodic):
-                A = self.periodic[idx - len(self.elements)]
+            case _ if idx < len(self.dirichlet) + len(self.periodic):
+                A = self.periodic[idx - len(self.dirichlet)]
                 return 1, A
 
-            case _ if idx < len(self.elements) + len(self.periodic) + len(self.colloc):
-                a = self.colloc[idx - len(self.elements) - len(self.periodic)]
+            case _ if idx < len(self.dirichlet) + len(self.periodic) + len(self.colloc):
+                a = self.colloc[idx - len(self.dirichlet) - len(self.periodic)]
                 return 2, a
 
             case _:
@@ -96,21 +98,24 @@ class PINNDataset(torch.utils.data.Dataset[TYPE]):
         return torch.utils.data.DataLoader(self, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn) # type: ignore
     
 
-def collate_fn(batch: list[TYPE]) -> tuple[tuple[Tensor, Tensor], list[list[Tensor]], Tensor]:
+def collate_fn(batch: list[TYPE]) -> tuple[ dirichlet_type, periodic_type, colloc_type ]:
 
-    batch_list: tuple[list[tuple[Tensor, Tensor]], list[list[Tensor]], list[Tensor]] = ([], [], [])
+    batch_list: tuple[list[dirichlet_type], list[periodic_type], list[colloc_type]] = ([], [], [])
 
     for item in batch:
         batch_list[item[0]].append(item) # type: ignore
 
-    elements: tuple[Tensor, Tensor] = (
-        torch.stack([a for _, (a, _) in batch_list[0]]),
-        torch.stack([u for _, (_, u) in batch_list[0]])
+    dirichlet: dirichlet_type = (
+        torch.stack([a for (a, _) in batch_list[0]]),
+        torch.stack([u for (_, u) in batch_list[0]])
     )
 
-    periodic = batch_list[1]
+    periodic: periodic_type = (
+        (torch.stack([a_1 for (a_1, _) in batch_list[1]]),
+         torch.stack([a_2 for (_, a_2) in batch_list[1]]))
+    )
 
-    colloc: Tensor = torch.stack(batch_list[2])
+    colloc: colloc_type = torch.stack(batch_list[2])
 
-    return elements, periodic, colloc
+    return dirichlet, periodic, colloc
 
