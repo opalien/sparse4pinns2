3503db9e092d33c787689d6f03f670f6f02c5368
Tib <NotAn@ema.il>
2025-06-10 07:39:09 +0200

    any pinn modified

--- examples/any/model.py
+++ examples/any/model.py
@@ -6,7 +6,7 @@ from torch import nn
 
 class AnyPINN(PINN):
     def __init__(self,  layers: Iterable[DLinear],
-                        pde: Callable[[PINN, Tensor, Tensor, int | None], Tensor],
+                        pde: Callable[[PINN, Tensor, Tensor], Tensor],
                         activation: type[nn.Module]=nn.Tanh, 
                         lmda:float =1.0) -> None:
         super().__init__(layers, activation, lmda)
@@ -14,6 +14,8 @@ class AnyPINN(PINN):
         self.pde_func = pde
 
 
-    def pde(self, u_pred: Tensor, a_in: Tensor, idx:int | None = None) -> Tensor:
-        return self.pde_func(self, u_pred, a_in, idx)
+    def pde(self, a: Tensor, u: Tensor) -> Tensor:
+        return self.pde_func(self, a, u)
         
+    
+    
